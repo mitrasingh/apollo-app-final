57313e138622d77d57dcf793c10595c785507672
Mitra Singh <mitra.coder@gmail.com>
2023-10-12 23:42:39 -0400

    refactoring code and transitioning to a css file rather than inline styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Nav, NavDropdown, Navbar, Image } from 'react-bootstrap'
+import { Col, Container, Nav, NavDropdown, Navbar, Image, Stack } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import { auth } from '../utils/firebase-config'
@@ -23,31 +23,38 @@ export const Navigation = () => {
 
     return (
         <Navbar bg="light" variant="light" className="px-5">
-            <Container fluid>
-                <Col xs lg="2">
-                    <Navbar.Brand as={Link} to="/">
-                        <img
-                            src="public/img/rocket.svg"
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                            alt="apollo logo"
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Brand className="fw-bold" as={Link} to="/">Apollo</Navbar.Brand>
+            {/* <Navbar collapseOnSelect expand="sm" bg="light" variant="light" className="px-5"> */}
+            {/* <Navbar.Toggle aria-controls="navbarScroll" data-bs-target="#navbarScroll" /> */}
+            <Container>
+
+                <Col className="d-flex justify-content-start">
+                    <Stack direction="horizontal">
+                        <Navbar.Brand as={Link} to="/">
+                            <img
+                                src="public/img/rocket.svg"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                                alt="apollo logo"
+                            />
+                        </Navbar.Brand>
+                        {/* <Navbar.Brand style={{ fontSize: "12px" }} className="fw-bold" as={Link} to="/">Apollo</Navbar.Brand> */}
+                    </Stack>
                 </Col>
 
-                <Col md="auto">
-                    <Nav style={{ fontSize: "11px" }} className="me-auto fw-bold mt-1">
-                        <Nav.Link as={Link} to="/" className="me-2">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/createtask">Create Task</Nav.Link>
-                        <Nav.Link as={Link} to="/shoutboard" className="ms-2">Shout Board</Nav.Link>
-                    </Nav>
+                <Col md="auto" className="d-flex justify-content-center text-nowrap">
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav style={{ fontSize: "10px" }} className="fw-bold mt-1">
+                            <Nav.Link as={Link} to="/" className="me-1">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/createtask">Create Task</Nav.Link>
+                            <Nav.Link as={Link} to="/shoutboard" className="ms-1">Shout Board</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Col>
 
-                <Col xs lg="2" className="d-flex justify-content-end">
+                <Col className="d-flex justify-content-end">
                     <Nav>
-                        <NavDropdown drop="down-centered" title="" menuVariant="light">
+                        <NavDropdown drop="down" title="" menuVariant="light">
                             <NavDropdown.Item style={{ fontSize: "9px" }} as={Link} to="/profile">Edit Profile</NavDropdown.Item>
                             <NavDropdown.Item style={{ fontSize: "9px" }} onClick={handleLogout}>Logout</NavDropdown.Item>
                         </NavDropdown>
@@ -62,9 +69,10 @@ export const Navigation = () => {
                                 src={userImage}
                                 roundedCircle />
                         </Navbar.Brand>
-                        <Nav.Link style={{ fontSize: "9px" }} className="fw-bold pt-3 ps-0">Hello, <strong>{user.firstName}</strong></Nav.Link>
+                        {/* <Nav.Link style={{ fontSize: "9px" }} className="fw-bold pt-3 ps-0">Hi, <strong>{user.firstName}</strong></Nav.Link> */}
                     </Nav>
                 </Col>
+
             </Container>
         </Navbar>
     )
