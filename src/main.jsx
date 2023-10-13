129593cadcf7372dff0daec920f6dcd1921e3e0d
Mitra Singh <mitra.coder@gmail.com>
2023-10-12 23:43:21 -0400

    imported css file

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./main.css";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
