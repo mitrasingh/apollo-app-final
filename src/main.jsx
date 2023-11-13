e39f1587a492c66e728dae0569e385d33c3f9e6e
Mitra Singh <mitra.coder@gmail.com>
2023-11-13 07:10:05 -0500

    BrowserRouter component removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/main.css";
@@ -11,9 +10,7 @@ import "../css/main.css";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 )
