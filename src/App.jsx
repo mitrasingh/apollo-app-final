cfd0b2a106d8fcad038a9e59735106862f1ef8d3
Mitra Singh <mitra.coder@gmail.com>
2023-11-13 07:05:07 -0500

    updated new react-router-dom structure and code has been refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { Routes, Route } from "react-router";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { CreateTask } from "./pages/CreateTask";
 import { Profile } from "./pages/Profile";
@@ -11,27 +16,14 @@ import { TopicDetails } from "./pages/TopicDetails";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { NotFound } from "./pages/NotFound";
 
-
-function App() {
-  return (
-    <Routes>
-      <Route element={<ProtectedRoute />} >
-        <Route path="/" element={<Home />} />
-      </Route>
-
-      <Route element={<ProtectedRoute />} >
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route element={<ProtectedRoute />}>
+        <Route index element={<Home />} />
         <Route path="createtask" element={<CreateTask />} />
-      </Route>
-
-      <Route element={<ProtectedRoute />} >
         <Route path="shoutboard" element={<Shoutboard />} />
-      </Route>
-
-      <Route element={<ProtectedRoute />} >
         <Route path="profile" element={<Profile />} />
-      </Route>
-
-      <Route element={<ProtectedRoute />} >
         <Route path="shoutboard/:id" element={<TopicDetails />} />
       </Route>
 
@@ -40,7 +32,13 @@ function App() {
       <Route path="forgotpassword" element={<ForgotPassword />} />
       <Route path="photoupload" element={<PhotoUpload />} />
       <Route path="*" element={<NotFound />} />
-    </Routes>
+    </Route>
+  )
+);
+
+function App() {
+  return (
+    <RouterProvider router={router} />
   );
 }
 
