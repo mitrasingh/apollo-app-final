67953ac1eaee4f4eab14fb442f242f47005e65b8
Mitra Singh <mitra.coder@gmail.com>
2023-11-20 19:08:08 -0500

    refactored code, made code more modular, move data fetching and rendering to a seperate component

--- src/pages/TopicBoard.jsx
+++ src/pages/TopicBoard.jsx
@@ -1,86 +1,45 @@
 import { Container } from "react-bootstrap";
-// import { useState } from "react";
-// import { Button } from "react-bootstrap";
-// import { collection, getDocs, query } from "firebase/firestore";
-// import { db } from "../utils/firebase-config";
-// import Spinner from 'react-bootstrap/Spinner';
-// import { TopicCard } from "../components/TopicCard";
-import TopicCards from "../components/TopicCards";
-import ErrorFallback from "../components/ErrorFallback";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
 import { ErrorBoundary } from "react-error-boundary"
-// import { CreateTopicForm } from "../components/CreateTopicForm";
+import ErrorFallbackTopics from "../components/ErrorFallbackTopics";
+import TopicCards from "../components/TopicCards";
+import CreateTopicForm from "../components/CreateTopicForm";
 import styles from "./TopicBoard.module.css";
 
 export const TopicBoard = () => {
-	// Current state of data fetched from getTopics function
-	// const [topicArray, setTopicArray] = useState([]);
+	// Invokes the display of CreateTopicForm.jsx component
+	const [isCreateTopic, setIsCreateTopic] = useState(false);
 
-	// Triggers the display component of CreateTopicForm.jsx when user invokes an action
-	// const [isCreateTopic, setIsCreateTopic] = useState(false);
+	// Refreshes topic list when user posts a new topic via CreateTopicForm.jsx
+	const [isTopicsRefreshed, setIsTopicsRefreshed] = useState(false);
 
-	// Triggers refresh topic list when user posts a new topic via CreateTopicForm.jsx
-	// const [isTopicsRefreshed, setIsTopicsRefreshed] = useState(false);
-
-	// State for displaying loader component
-	// const [isLoading, setIsLoading] = useState(true);
-
-	// Function that fetches data from database by querying "topics" collection
-	// useEffect(() => {
-	// 	const fetchTopics = async () => {
-	// 		try {
-	// 			const dbRef = collection(db, "topics")
-	// 			const topicsData = await getDocs(query(dbRef))
-	// 			const topicsMap = topicsData.docs.map((doc) => ({ ...doc.data(), topicId: doc.id }))
-	// 			setTopicArray(topicsMap);
-	// 		} catch (error) {
-	// 			console.log(error);
-	// 		} finally {
-	// 			setTimeout(() => {
-	// 				setIsLoading(false);
-	// 			}, 100)
-	// 		}
-	// 	};
-	// 	fetchTopics();
-	// }, [isTopicsRefreshed]);
-
-	// onClick function for displaying component CreateTopicForm.jsx
-	// const handleCreateTopic = () => {
-	// 	!isCreateTopic ? setIsCreateTopic(true) : setIsCreateTopic(false);
-	// };
+	// Displays component CreateTopicForm.jsx w
+	const handleCreateTopic = () => {
+		!isCreateTopic ? setIsCreateTopic(true) : setIsCreateTopic(false);
+	};
 
 	return (
 		<Container className={styles.customContainer}>
-			{/* {isLoading ?
-				<div className="d-flex justify-content-center align-items-center vh-100">
-					<Spinner animation="border" variant="warning" />
-				</div>
-				:
-				<> */}
 			<p className="fs-2 fw-bold d-flex justify-content-center text-light">Topic Board</p>
-			{/* <Button
+			<Button
 				className={`d-flex align-items-center justify-content-center fs-6 fw-bold text-light ms-3 mb-2 ${styles.customBtn}`}
 				variant="primary"
 				onClick={handleCreateTopic}
 			>
 				{!isCreateTopic ? "+ Create Topic" : "- Close"}
-			</Button> */}
+			</Button>
 
-			{/* {isCreateTopic ? (
+			{isCreateTopic ? (
 				<CreateTopicForm
 					setIsCreateTopic={setIsCreateTopic}
 					setIsTopicsRefreshed={setIsTopicsRefreshed}
 				/>
-			) : null} */}
-			<ErrorBoundary
-				FallbackComponent={ErrorFallback}
-			>
-				<TopicCards />
+			) : null}
+
+			<ErrorBoundary FallbackComponent={ErrorFallbackTopics}>
+				<TopicCards isTopicsRefreshed={isTopicsRefreshed} />
 			</ErrorBoundary>
-			{/* {topicArray.map((topic) => {
-						return <TopicCard topic={topic} key={topic.topicId} />;
-					})} */}
-			{/* </> */}
-			{/* } */}
 		</Container>
 	);
 };
