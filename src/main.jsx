720e8d98fe348a1ce48fb19bd3704ce57bd17366
Mitra Singh <mitra.coder@gmail.com>
2023-11-20 19:03:39 -0500

    react toastify container added to be used throughout the app

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/main.css";
 
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer />
     </Provider>
   </React.StrictMode>
 )
