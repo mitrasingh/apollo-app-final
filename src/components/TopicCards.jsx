b789c2067687527be975b9b0e493510a20814fbc
Mitra Singh <mitra.coder@gmail.com>
2023-11-20 19:09:43 -0500

    created for to be more modular, fetches dand renders each individual topic card

---
+++ src/components/TopicCards.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { db } from "../utils/firebase-config";
+import { useErrorBoundary } from "react-error-boundary";
+import Spinner from 'react-bootstrap/Spinner';
+import TopicCard from "../components/TopicCard";
+import PropTypes from "prop-types";
+
+export const TopicCards = ({ isTopicsRefreshed }) => {
+    // Current state of data fetched from fetchTopics function
+    const [topicArray, setTopicArray] = useState([]);
+
+    // Catches error and returns to error boundary component (error component invoked in TopicBoard.jsx)
+    const { showBoundary } = useErrorBoundary();
+
+    // State for displaying loader component
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Function that fetches data from database by querying "topics" collection
+    useEffect(() => {
+        const fetchTopics = async () => {
+            try {
+                const dbRef = collection(db, "topics");
+                const queryOrderDate = query(dbRef, orderBy("datePosted", "desc"));
+                const topicsData = await getDocs(queryOrderDate);
+                const topicsMap = topicsData.docs.map((doc) => ({ ...doc.data(), topicId: doc.id }))
+                setTopicArray(topicsMap);
+            } catch (error) {
+                showBoundary(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchTopics();
+    }, [isTopicsRefreshed]);
+
+    return (
+        <>
+            {isLoading ?
+                <div className="d-flex justify-content-center mt-4">
+                    <Spinner animation="border" variant="warning" />
+                </div>
+                :
+                <>
+                    {topicArray.map(topic => {
+                        return <TopicCard topic={topic} key={topic.topicId} />;
+                    })}
+                </>
+            }
+        </>
+    );
+};
+
+TopicCards.propTypes = {
+    isTopicsRefreshed: PropTypes.bool.isRequired
+}
+
+export default TopicCards;
