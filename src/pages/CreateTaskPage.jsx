758dd95791691b2a03a02d28f70b86837e925fef
Mitra Singh <mitra.coder@gmail.com>
2023-11-24 18:18:52 -0500

    styles added to component

--- src/pages/CreateTaskPage.jsx
+++ src/pages/CreateTaskPage.jsx
@@ -1,11 +1,13 @@
+import { Container } from "react-bootstrap";
 import CreateTask from "../components/CreateTask";
+import styles from "./CreateTaskPage.module.css";
 
-export const CreateTaskPage = () => {
+const CreateTaskPage = () => {
 	return (
-		<>
+		<Container className={`text-light fs-6 px-4 ${styles.formContainer}`}>
 			<CreateTask />
-		</>
-	);
+		</Container>
+	)
 };
 
 export default CreateTaskPage;
