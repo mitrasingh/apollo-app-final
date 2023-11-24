eaf21450bae4365ef009fedccaf19d08f83afca3
Mitra Singh <mitra.coder@gmail.com>
2023-11-24 18:33:03 -0500

    now created as its own component, added notification when topic is deleted (successfull or unsuccessfull)

---
+++ src/components/TopicPost.jsx
@@ -0,0 +1,215 @@
+import { useState, useEffect } from "react";
+import { Card, Row, Col, Image, Stack, Dropdown, CloseButton } from "react-bootstrap";
+import { doc, getDoc, collection, query, getCountFromServer, where, deleteDoc } from "firebase/firestore";
+import { getStorage, getDownloadURL, ref } from "firebase/storage";
+import { useParams, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { db } from "../utils/firebase-config";
+import { toast } from 'react-toastify';
+import * as dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import PropTypes from "prop-types";
+import EditTopic from "./EditTopic";
+import DeleteModal from "./DeleteModal";
+import Like from "./Like";
+
+const TopicPost = ({ isTopicRefreshed, setIsTopicRefreshed, isCommentsRefreshed }) => {
+    // useParams, creates a dynamic page using the topicId property from its fetched document within the "topics" collection in database
+    // This shared id also specifies the specific document to query within the "topics" collection of the database
+    const { id } = useParams();
+
+    // Function allows user to navigate elsewhere
+    const navigate = useNavigate();
+
+    // Stores document data fetched from database via fetchTopicData function
+    const [topic, setTopic] = useState([]);
+
+    // Displays numbers of replies (or how many documents within "comments" subcollection)
+    const [numOfComments, setNumOfComments] = useState("");
+
+    // Stores the formatted date
+    const [displayTimeStamp, setDisplayTimeStamp] = useState("");
+
+    // Stores user photo URL fetched from firebase storage via fetchTopicData function
+    const [userPhoto, setUserPhoto] = useState("");
+
+    // Confirms user submitted change to topic when set to true
+    const [isTopicEdited, setIsTopicEdited] = useState(false)
+
+    // Display edit fields for the topic description when set to true
+    const [isEditTopicDisplayed, setIsEditTopicDisplayed] = useState(false);
+
+    // Firebase storage method and reference (used for fetching user photo url based off of userId prop)
+    const storage = getStorage();
+    const storageRef = ref(storage);
+
+    // Redux state properties of current user (sets default properties when posting a comment)
+    const currentUser = useSelector((state) => state.user);
+
+    // Fetch data of specific document id (via useParams()) from "topics" collection in firestore database
+    useEffect(() => {
+        const fetchTopicData = async () => {
+            try {
+                const docRef = doc(db, "topics", id);
+                const docSnap = await getDoc(docRef);
+
+                if (!docSnap.exists()) {
+                    navigate("/error");
+                }
+
+                if (docSnap.exists()) {
+                    const data = docSnap.data();
+                    const userPhotoURL = await getDownloadURL(
+                        ref(storageRef, `user-photo/${data.userId}`)
+                    );
+                    setUserPhoto(userPhotoURL);
+                    setTopic(data);
+                    // Conversion of firestore timestamp to dayjs fromNow method
+                    dayjs.extend(relativeTime);
+                    const convertTimeStamp = data.datePosted.toDate();
+                    const dateRelativeTime = dayjs(convertTimeStamp).fromNow();
+                    setDisplayTimeStamp(dateRelativeTime);
+                }
+                // Returns the total number of documents within the "comments" subcollection
+                const commentsToQuery = query(
+                    collection(db, "comments"),
+                    where("topicId", "==", id)
+                );
+                const snapshot = await getCountFromServer(commentsToQuery);
+                setNumOfComments(snapshot.data().count);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchTopicData();
+    }, [isTopicRefreshed, isCommentsRefreshed]);
+
+    // Deletes the entire topic including it's comments
+    const [isVisible, setIsVisible] = useState(false);
+    const handleShow = () => setIsVisible(true);
+    const handleDeleteTopic = async () => {
+        const documentRef = doc(db, "topics", id);
+        try {
+            await deleteDoc(documentRef);
+            navigate("/topicboard");
+            setIsVisible(false);
+            toast.success('Topic has been deleted!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        } catch (error) {
+            console.log(error);
+            toast.error('Could not delete topic!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+        }
+    };
+
+    // Navigates user back to the shoutboard page
+    const handleCloseTopic = () => {
+        navigate("/topicboard");
+    };
+
+    return (
+        <Card className="text-light">
+            <Card.Header className="fs-6">
+                <Row>
+                    <Col xs={10}>
+                        <Stack direction="horizontal" gap={2}>
+                            <Image
+                                height="30px"
+                                width="30px"
+                                src={userPhoto}
+                                roundedCircle
+                            />
+                            <Stack direction="vertical">
+                                <Card.Text className="my-0">Posted by:</Card.Text>
+                                <Card.Text>{topic.firstName} {topic.lastName}</Card.Text>
+                            </Stack>
+                        </Stack>
+                    </Col>
+                    <Col xs={2} className="d-flex justify-content-end">
+                        <Stack direction="horizontal" gap={2}>
+                            {topic.userId === currentUser.userId ? (
+                                <>
+                                    <Dropdown>
+                                        <Dropdown.Toggle
+                                            className="d-flex align-items-center text-light"
+                                            split
+                                            variant="primary"
+                                            id="dropdown-split-basic"
+                                        ></Dropdown.Toggle>
+
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item onClick={() => setIsEditTopicDisplayed(true)}>
+                                                Edit
+                                            </Dropdown.Item>
+                                            <Dropdown.Item onClick={handleShow}>
+                                                Delete
+                                            </Dropdown.Item>
+                                            {isVisible ? (
+                                                <DeleteModal
+                                                    handleDelete={handleDeleteTopic}
+                                                    setIsVisible={setIsVisible}
+                                                    isVisible={isVisible}
+                                                    type={"topic"}
+                                                />
+                                            ) : null}
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </>
+                            ) : null}
+                            <CloseButton onClick={handleCloseTopic} />
+                        </Stack>
+                    </Col>
+                </Row>
+            </Card.Header>
+
+            <Card.Body>
+                <h5 className="fs-3 fw-bold">{topic.title}</h5>
+                <p className="fs-6">
+                    {isTopicEdited ? `Updated post` : `Posted`} {displayTimeStamp}  |  {numOfComments}
+                    {numOfComments === 1 ? " Reply" : " Replies"}
+                </p>
+
+                {isEditTopicDisplayed ? (
+                    <EditTopic
+                        setIsEditTopicDisplayed={setIsEditTopicDisplayed}
+                        description={topic.description}
+                        id={id}
+                        setIsTopicRefreshed={setIsTopicRefreshed}
+                        setIsTopicEdited={setIsTopicEdited}
+                    />
+                ) : (
+                    <p className="mt-4 fs-5">
+                        {topic.description}
+                    </p>
+                )}
+            </Card.Body>
+            <Card.Body className="py-0">
+                <Like docId={id} />
+            </Card.Body>
+        </Card>
+    )
+}
+
+TopicPost.propTypes = {
+    isTopicRefreshed: PropTypes.bool.isRequired,
+    setIsTopicRefreshed: PropTypes.func.isRequired,
+    isCommentsRefreshed: PropTypes.bool.isRequired,
+};
+
+export default TopicPost;
