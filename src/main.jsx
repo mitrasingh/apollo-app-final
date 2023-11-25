b053f567abc1b6115a78dc8e3d448c3959cfcd34
Mitra Singh <mitra.coder@gmail.com>
2023-11-25 18:21:45 -0500

    added properties to toast container

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      <ToastContainer />
+      <ToastContainer
+        className="fs-5 mt-4"
+        autoClose={2500}
+        hideProgressBar={true}
+        theme="dark"
+      />
     </Provider>
   </React.StrictMode>
 )
