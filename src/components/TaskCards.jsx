9d391a3108480c0a96a2e1f167caa22d6b109975
Mitra Singh <mitra.coder@gmail.com>
2023-11-28 22:24:06 -0500

    created a new component which handles data fetch and maps out to individual task card

--- src/components/TaskCards.jsx
+++ src/components/TaskCards.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "../utils/firebase-config";
+import { useErrorBoundary } from "react-error-boundary";
 import Spinner from 'react-bootstrap/Spinner';
 import PropTypes from "prop-types";
 import TaskCard from "../components/TaskCard";
 
-
-const TaskCards = ({ setTaskArray, setTaskArrayFilter, taskArrayFilter, refreshTasksHandle }) => {
+const TaskCards = ({ setTaskArray, setTaskArrayFilter, taskArrayFilter }) => {
 
     // State for displaying loader component
     const [isLoading, setIsLoading] = useState(true);
+
+    // Catches error and returns to error boundary component (error component in parent (TopicBoard.jsx)
+    const { showBoundary } = useErrorBoundary();
 
     // Fetch data and map each task into state variables
     const fetchTasks = async () => {
@@ -23,7 +26,8 @@ const TaskCards = ({ setTaskArray, setTaskArrayFilter, taskArrayFilter, refreshT
             setTaskArray(tasksMap);
             setTaskArrayFilter(tasksMap);
         } catch (error) {
-            console.log(error);
+            console.log(`Error: ${error.message}`);
+            showBoundary(error);
         } finally {
             setIsLoading(false);
         }
@@ -32,7 +36,6 @@ const TaskCards = ({ setTaskArray, setTaskArrayFilter, taskArrayFilter, refreshT
     useEffect(() => {
         fetchTasks();
     }, []);
-
 
     return (
         <>
@@ -45,7 +48,7 @@ const TaskCards = ({ setTaskArray, setTaskArrayFilter, taskArrayFilter, refreshT
                     {taskArrayFilter.map((task) => {
                         return (
                             <TaskCard
-                                refreshTasksHandle={refreshTasksHandle}
+                                fetchTasks={fetchTasks}
                                 task={task}
                                 key={task.taskId}
                             />
@@ -61,7 +64,6 @@ TaskCards.propTypes = {
     setTaskArray: PropTypes.func.isRequired,
     setTaskArrayFilter: PropTypes.func.isRequired,
     taskArrayFilter: PropTypes.array.isRequired,
-    refreshTasksHandle: PropTypes.func.isRequired,
 };
 
 export default TaskCards;
