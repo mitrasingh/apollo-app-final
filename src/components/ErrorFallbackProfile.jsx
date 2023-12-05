beecfa0c85c4271d4fa9264c1e971b77010fc892
Mitra Singh <mitra.coder@gmail.com>
2023-12-04 21:31:26 -0500

    added component which will be displayed if any error occurs during data fetch of ProfileForm component

---
+++ src/components/ErrorFallbackProfile.jsx
@@ -0,0 +1,49 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import styles from "./ErrorFallbackTopics.module.css";
+
+export const ErrorFallbackProfile = ({ resetErrorBoundary }) => {
+
+    return (
+        <Container className={styles.customContainer}>
+            <Row>
+                <Col className="d-flex justify-content-center mt-4">
+                    <div className="text-light fs-4 fw-bold text-center">Ground control, profile details not loading!</div>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-flex justify-content-center">
+                    <Button
+                        className="ms-2 mt-2 fs-6 text-light fw-bold mt-3 mb-4"
+                        variant="primary"
+                        size="sm"
+                        type="submit"
+                        onClick={resetErrorBoundary}
+                    >
+                        Retry
+                    </Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-flex justify-content-center mt-4 fs-6">
+                    <p className="text-center text-light">
+                        Check out the&nbsp;
+                        <Link
+                            className="fw-bold"
+                            to="/topicboard"
+                        >
+                            Topic Board
+                        </Link>
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+ErrorFallbackProfile.propTypes = {
+    resetErrorBoundary: PropTypes.func.isRequired,
+};
+
+export default ErrorFallbackProfile;
