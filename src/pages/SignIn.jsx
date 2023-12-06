8c45ec4dc7369a31a699812f9355610155f43053
Mitra Singh <mitra.coder@gmail.com>
2023-12-05 22:20:47 -0500

    removed alert component from bootstrap, removed alert message state, added toast notifications and error logic to the handleLogin function

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Button, Card, Container, Form, Alert, Stack, Image } from "react-bootstrap";
+import { Button, Card, Container, Form, Stack, Image } from "react-bootstrap";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -7,12 +6,10 @@ import { loginUser } from "../features/user/userSlice";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../utils/firebase-config";
 import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify';
 import styles from "./SignIn.module.css";
 
 export const SignIn = () => {
-    // Invokes error message if functionality fails
-    const [isAlert, isSetAlert] = useState(false);
-    const [alertMessage, setAlertMessage] = useState("");
 
     // React Hook Form
     const form = useForm();
@@ -40,11 +37,18 @@ export const SignIn = () => {
                     email: auth.currentUser.email,
                 })
             );
+            toast.info("Welcome to Apollo!");
             navigate("/");
         } catch (error) {
-            console.log(error);
-            isSetAlert(true);
-            setAlertMessage(error.code);
+            if (error.message.includes("user-not-found")) {
+                toast.error("User not found!")
+            }
+            else if (error.message.includes("wrong-password")) {
+                toast.error("Password is incorrect!")
+            } else {
+                toast.error("Sorry, we are having some technical issues!")
+            }
+            console.log(`Error: ${error.message}`);
         }
     };
 
@@ -65,112 +69,100 @@ export const SignIn = () => {
                     email: auth.currentUser.email,
                 })
             );
+            toast.info("Welcome to Apollo!");
             navigate("/");
         } catch (error) {
-            console.log(error);
-            isSetAlert(true);
-            setAlertMessage(error.code);
+            toast.error("Sorry, we are having some technical issues!")
+            console.log(`Error: ${error.message}`);
         }
     };
 
     return (
-        <>
-            {isAlert ? (
-                <Alert variant="primary" className="text-center" data-bs-theme="dark">
-                    <Alert.Heading className="fw-bold">There is an error!</Alert.Heading>
-                    <p>Reason: {alertMessage}</p>
-                    <Link className="fs-5 fw-bold text-light" onClick={() => isSetAlert(false)}>
-                        Close
-                    </Link>
-                </Alert>
-            ) : null}
-
-            <Container className={`text-light fs-6 ${styles.customContainer}`}>
-                <h6 className="text-center">Welcome to</h6>
-                <Stack direction="horizontal" gap={2} className="d-flex justify-content-center">
-                    <Image
-                        src="public/img/rocket_white.svg"
-                        width="50"
-                        height="50"
-                        className="d-inline-block align-top"
-                        alt="apollo logo"
-                    />
-                    <h1 className="fw-bold">Apollo</h1>
-                </Stack>
-                <Form onSubmit={handleSubmit(handleLogin)} noValidate>
-                    <Card className="mt-4 p-4 text-light" >
-                        <h4 className="fw-bold text-center">Sign In</h4>
-                        <p className="text-center">
-                            Not registered?&nbsp;
-                            <Link
-                                className="fw-bold"
-                                to="/signup"
-                            >
-                                Sign Up
-                            </Link>
-                        </p>
-
-                        <p className="fw-bold mb-1">Email Address</p>
-                        <Form.Group className="mb-2">
-                            <Form.Control
-                                className="shadow-none fs-6"
-                                type="text"
-                                placeholder="Enter email"
-                                {...register("email", {
-                                    required: {
-                                        value: true,
-                                        message: "Email is required!"
-                                    },
-                                    pattern: {
-                                        value: emailRegex,
-                                        message: "Invalid email format!"
-                                    }
-                                })}
-                            />
-                            <p className="mt-2">{errors.email?.message}</p>
-                        </Form.Group>
-
-                        <p className="fw-bold mb-1">Password</p>
-                        <Form.Group className="mb-2">
-                            <Form.Control
-                                className="shadow-none fs-6"
-                                type="password"
-                                placeholder="Enter password"
-                                {...register("password", {
-                                    required: {
-                                        value: true,
-                                        message: "Password is required!"
-                                    }
-                                })}
-                            />
-                            <p className="mt-2">{errors.password?.message}</p>
-                        </Form.Group>
-
-                        <Button
-                            className="fw-bold text-light fs-5"
-                            variant="primary"
-                            size="sm"
-                            type="submit"
-                        >
-                            Login
-                        </Button>
-
+        <Container className={`text-light fs-6 ${styles.customContainer}`}>
+            <h6 className="text-center">Welcome to</h6>
+            <Stack direction="horizontal" gap={2} className="d-flex justify-content-center">
+                <Image
+                    src="public/img/rocket_white.svg"
+                    width="50"
+                    height="50"
+                    className="d-inline-block align-top"
+                    alt="apollo logo"
+                />
+                <h1 className="fw-bold">Apollo</h1>
+            </Stack>
+            <Form onSubmit={handleSubmit(handleLogin)} noValidate>
+                <Card className="mt-4 p-4 text-light" >
+                    <h4 className="fw-bold text-center">Sign In</h4>
+                    <p className="text-center">
+                        Not registered?&nbsp;
                         <Link
-                            className="mt-3 text-center"
-                            as={Link}
-                            to="/forgotpassword"
+                            className="fw-bold"
+                            to="/signup"
                         >
-                            Forgot password?
+                            Sign Up
                         </Link>
-                    </Card>
-                </Form>
-                <p className="text-center mt-4">
-                    Sign In as {" "}
-                    <Link className="fw-bold" onClick={handleGuestLogin}>
-                        Guest
+                    </p>
+
+                    <p className="fw-bold mb-1">Email Address</p>
+                    <Form.Group className="mb-2">
+                        <Form.Control
+                            className="shadow-none fs-6"
+                            type="text"
+                            placeholder="Enter email"
+                            {...register("email", {
+                                required: {
+                                    value: true,
+                                    message: "Email is required!"
+                                },
+                                pattern: {
+                                    value: emailRegex,
+                                    message: "Invalid email format!"
+                                }
+                            })}
+                        />
+                        <p className="mt-2">{errors.email?.message}</p>
+                    </Form.Group>
+
+                    <p className="fw-bold mb-1">Password</p>
+                    <Form.Group className="mb-2">
+                        <Form.Control
+                            className="shadow-none fs-6"
+                            type="password"
+                            placeholder="Enter password"
+                            {...register("password", {
+                                required: {
+                                    value: true,
+                                    message: "Password is required!"
+                                }
+                            })}
+                        />
+                        <p className="mt-2">{errors.password?.message}</p>
+                    </Form.Group>
+
+                    <Button
+                        className="fw-bold text-light fs-5"
+                        variant="primary"
+                        size="sm"
+                        type="submit"
+                    >
+                        Login
+                    </Button>
+
+                    <Link
+                        className="mt-3 text-center"
+                        as={Link}
+                        to="/forgotpassword"
+                    >
+                        Forgot password?
                     </Link>
-                </p>
-            </Container >
-        </>
+                </Card>
+            </Form>
+            <p className="text-center mt-4">
+                Sign In as {" "}
+                <Link className="fw-bold" onClick={handleGuestLogin}>
+                    Guest
+                </Link>
+            </p>
+        </Container >
     );
 };
