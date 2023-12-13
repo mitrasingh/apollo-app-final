dd9d572a265cff440113a750baca7a9ef27cb63f
Mitra Singh <mitra.coder@gmail.com>
2023-12-12 23:29:21 -0500

    updated import of CreateTaskForm

--- src/pages/CreateTask/CreateTaskPage.jsx
+++ src/pages/CreateTask/CreateTaskPage.jsx
@@ -1,11 +1,11 @@
 import { Container } from "react-bootstrap";
-import CreateTask from "../../components/CreateTask";
+import CreateTaskForm from "./CreateTaskForm/CreateTaskForm";
 import styles from "./CreateTaskPage.module.css";
 
 const CreateTaskPage = () => {
 	return (
 		<Container className={`text-light fs-6 px-4 ${styles.formContainer}`}>
-			<CreateTask />
+			<CreateTaskForm />
 		</Container>
 	)
 };
