4877f63e2b10e9983137d5c027a1c564ef45c763
Mitra Singh <mitra.coder@gmail.com>
2023-12-24 00:21:36 -0500

    refactored code, removed unnecessary code that is no longer needed

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,11 +1,9 @@
 import { Outlet, useLocation, Navigate } from "react-router-dom";
-import { auth, db } from "../../utils/firebase-config";
+import { auth } from "../../utils/firebase-config";
 import { useState, useEffect } from 'react';
 import { getAuth, signOut } from 'firebase/auth';
-import { getStorage, ref, getDownloadURL } from "firebase/storage"
 import { useDispatch } from 'react-redux';
-import { doc, getDoc } from 'firebase/firestore';
-import { loginUser, logoutUser } from "../../store/user/userSlice";
+import { logoutUser } from "../../store/user/userSlice";
 import Navigation from "../Navigation/Navigation";
 
 const ProtectedRoute = () => {
@@ -14,11 +12,8 @@ const ProtectedRoute = () => {
   const location = useLocation();
 
   // Redux management 
+  // const userState = useSelector((state) => state.user);
   const dispatch = useDispatch(); // Redux function which dispatches actions to Redux store
-
-  // Reference to firebase storage
-  const storage = getStorage();
-  const storageRef = ref(storage);
 
   // State variable which verifies the authentication of a user through firebase
   const [isLoggedIn, setIsLoggedIn] = useState(auth);
@@ -34,21 +29,6 @@ const ProtectedRoute = () => {
       try {
         if (user) {
           setIsLoggedIn(auth);
-          const userCustomPhotoRef = `user-photo/${user.uid}`;
-          const userTempPhotoRef = "user-photo/temporaryphoto.jpeg";
-          const photoRefCondition = !userCustomPhotoRef ? userTempPhotoRef : userCustomPhotoRef;
-          const userPhotoURL = await getDownloadURL(ref(storageRef, photoRefCondition));
-          const docRef = doc(db, "users", user.uid)
-          const docSnap = await getDoc(docRef)
-          const data = docSnap.data()
-          dispatch(loginUser({
-            userId: user.uid,
-            userPhoto: userPhotoURL,
-            firstName: user.displayName,
-            lastName: data.lastname,
-            title: data.title,
-            email: user.email
-          }))
         }
       } catch (error) {
         console.log(`Error: ${error.message}`);
