17538e15e2aa49f050431a97f25a829a226a06ea
Mitra Singh <mitra.coder@gmail.com>
2023-12-27 15:25:36 -0500

    added dispatch for if user is logged in, this will keep user data incase window is refreshed manually

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -3,7 +3,10 @@ import { auth } from "../../utils/firebase-config";
 import { useState, useEffect } from 'react';
 import { getAuth, signOut } from 'firebase/auth';
 import { useDispatch } from 'react-redux';
+import { db } from "../../utils/firebase-config";
 import { logoutUser } from "../../store/user/userSlice";
+import { loginUser } from "../../store/user/userSlice";
+import { doc, getDoc } from "firebase/firestore";
 import Navigation from "../Navigation/Navigation";
 import ScrollToTop from "../ScrollToTop/ScrollToTop";
 
@@ -13,7 +16,6 @@ const ProtectedRoute = () => {
   const location = useLocation();
 
   // Redux management 
-  // const userState = useSelector((state) => state.user);
   const dispatch = useDispatch(); // Redux function which dispatches actions to Redux store
 
   // State variable which verifies the authentication of a user through firebase
@@ -30,6 +32,20 @@ const ProtectedRoute = () => {
       try {
         if (user) {
           setIsLoggedIn(auth);
+          const docRef = doc(db, "users", auth.currentUser.uid);
+          const docSnap = await getDoc(docRef);
+          const userData = docSnap.data();
+          // Assigns default values to Redux user state
+          dispatch(
+            loginUser({
+              userId: auth.currentUser.uid,
+              userPhoto: userData.userPhoto,
+              firstName: userData.firstname,
+              lastName: userData.lastname,
+              title: userData.title,
+              email: userData.email,
+            })
+          );
         }
       } catch (error) {
         console.log(`Error: ${error.message}`);
