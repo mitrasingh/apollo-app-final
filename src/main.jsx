959c77d2c06076b75f4ebdeb7b62373f093de123
Mitra Singh <mitra.coder@gmail.com>
2024-01-29 18:43:22 -0500

    import new component which now holds react toastify container

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
+import ToastContainerCondition from "../src/components/ToastContainerCondition/ToastContainerCondition";
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/main.css";
@@ -12,14 +13,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      <ToastContainer
+      <ToastContainerCondition />
+      {/* <ToastContainer
         className="fs-5 mt-4"
         position="bottom-center"
         autoClose={2500}
         hideProgressBar={false}
         theme="dark"
         closeOnClick
-      />
+      /> */}
     </Provider>
   </React.StrictMode>
 )
