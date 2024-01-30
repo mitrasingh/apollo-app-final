4b2c362ed7dc8e4b0650d0fa2df46442222845a7
Mitra Singh <mitra.coder@gmail.com>
2024-01-29 19:19:07 -0500

    added comments to component

--- src/components/ToastContainerCondition/ToastContainerCondition.jsx
+++ src/components/ToastContainerCondition/ToastContainerCondition.jsx
@@ -2,8 +2,11 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const ToastContainerCondition = () => {
+
+    // Condition checks to see if user is on mobile or viewport less than 600px
     const isMobile = window.innerWidth <= 600;
     const toastPosition = isMobile ? 'bottom-center' : 'top-right';
+
     return (
         <ToastContainer
             className="fs-5 mt-4"
