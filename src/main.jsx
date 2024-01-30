5c2642734bd2dcb6bb2f4198a7ad93ccd361affd
Mitra Singh <mitra.coder@gmail.com>
2024-01-29 19:18:45 -0500

    removed unwanted import statements

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ToastContainerCondition from "./components/ToastContainerCondition/ToastContainerCondition";
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-// import { ToastContainer } from 'react-toastify';
-import ToastContainerCondition from "../src/components/ToastContainerCondition/ToastContainerCondition";
-import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/main.css";
 
@@ -14,14 +12,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <App />
       <ToastContainerCondition />
-      {/* <ToastContainer
-        className="fs-5 mt-4"
-        position="bottom-center"
-        autoClose={2500}
-        hideProgressBar={false}
-        theme="dark"
-        closeOnClick
-      /> */}
     </Provider>
   </React.StrictMode>
 )
