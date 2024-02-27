083444cd58a560b1f7c99941f3cc3401ffd83e2a
Mitra Singh <mitra.coder@gmail.com>
2024-02-27 14:06:01 -0500

    refactored code and mapped out filter based on array of string values

--- src/pages/Home/FilterTasksButton/FilterTasksButton.jsx
+++ src/pages/Home/FilterTasksButton/FilterTasksButton.jsx
@@ -4,32 +4,32 @@ import PropTypes from "prop-types";
 // Props from Home.jsx
 const FilterTasksButton = ({ filterLaterHandle, filterSoonHandle, filterPriorityHandle, filterStatusHandle }) => {
   return (
-    <>
-      <Dropdown as={ButtonGroup}>
-        <Dropdown.Toggle
-          className="d-flex align-items-center text-light fs-6 fw-bold"
-          variant="primary"
-          id="dropdown-split-basic"
-        >
-          Filter
-        </Dropdown.Toggle>
+    <Dropdown as={ButtonGroup}>
+      <Dropdown.Toggle
+        className="d-flex align-items-center text-light fs-6 fw-bold"
+        variant="primary"
+        id="dropdown-split-basic"
+      >
+        Filter
+      </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={filterSoonHandle}>Due Soon</Dropdown.Item>
-          <Dropdown.Item onClick={filterLaterHandle}>Due Later</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item onClick={() => filterPriorityHandle("Urgent")}>Urgent</Dropdown.Item>
-          <Dropdown.Item onClick={() => filterPriorityHandle("High")}>High</Dropdown.Item>
-          <Dropdown.Item onClick={() => filterPriorityHandle("Medium")}>Medium</Dropdown.Item>
-          <Dropdown.Item onClick={() => filterPriorityHandle("Low")}>Low</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item onClick={() => filterStatusHandle("In Progress")}>In Progress</Dropdown.Item>
-          <Dropdown.Item onClick={() => filterStatusHandle("Done")}>Done</Dropdown.Item>
-          <Dropdown.Item onClick={() => filterStatusHandle("On Hold")}>On Hold</Dropdown.Item>
-          <Dropdown.Item onClick={() => filterStatusHandle("Cancelled")}>Cancelled</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={filterSoonHandle}>Due Soon</Dropdown.Item>
+        <Dropdown.Item onClick={filterLaterHandle}>Due Later</Dropdown.Item>
+        <Dropdown.Divider />
+        {['Urgent', 'High', 'Medium', 'Low'].map((priority) => (
+          <Dropdown.Item key={priority} onClick={() => filterPriorityHandle(priority)}>
+            {priority}
+          </Dropdown.Item>
+        ))}
+        <Dropdown.Divider />
+        {['In Progress', 'Done', 'On Hold', 'Cancelled'].map((status) => (
+          <Dropdown.Item key={status} onClick={() => filterStatusHandle(status)}>
+            {status}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
   );
 };
 
