1ca4dd6edcc4494f84078ac26685eb061adb9799
Mitra Singh <mitra.coder@gmail.com>
2024-03-03 11:46:26 -0500

    moved page title to component from CreateTaskForm component

--- src/pages/CreateTask/CreateTaskPage.jsx
+++ src/pages/CreateTask/CreateTaskPage.jsx
@@ -5,6 +5,7 @@ import styles from "./CreateTaskPage.module.css";
 const CreateTaskPage = () => {
 	return (
 		<Container className={`text-light fs-6 px-4 ${styles.formContainer}`}>
+			<p className="fs-2 fw-bold d-flex justify-content-center text-light">Create Task</p>
 			<CreateTaskForm />
 		</Container>
 	)
