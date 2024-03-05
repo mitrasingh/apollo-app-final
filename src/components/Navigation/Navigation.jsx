3ec8459f979e6f21c880712094f8fb22a2f01e8d
Mitra Singh <mitra.coder@gmail.com>
2024-03-04 23:41:50 -0500

    navigation now closes when user clicks outside of ref area

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,7 @@
 import { Container, Nav, Navbar, Image, Form, Tooltip, OverlayTrigger } from 'react-bootstrap';
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../utils/firebase-config";
@@ -12,6 +12,23 @@ import LogoutModal from "../Modals/LogoutModal";
 import styles from "./Navigation.module.css";
 
 const Navigation = () => {
+    const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+    const navbarRef = useRef(null);
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+                setIsNavbarOpen(false); // Close the Navbar if clicked outside
+            }
+        };
+
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, []);
+
     // Fetching user values from Redux
     const user = useSelector((state) => state.user);
 
@@ -38,8 +55,14 @@ const Navigation = () => {
     const [isLogoutModalVisible, setLogOutModalVisible] = useState(false);
     const handleVisible = () => setLogOutModalVisible(true);
 
+    // const [showMenu, setShowMenu] = useState(false);
+
+    // const handleMenuToggle = () => {
+    //     setShowMenu(!showMenu);
+    // };
+
     return (
-        <Navbar collapseOnSelect fixed="top" bg="info" variant="dark" expand="lg" className={`px-5 d-flex flex-row ${styles.customNav}`}>
+        <Navbar collapseOnSelect fixed="top" bg="info" variant="dark" expand="lg" className={`px-5 d-flex flex-row ${styles.customNav}`} ref={navbarRef}>
             <Container className={styles.customContainer}>
                 <Navbar.Brand as={Link} to="/home" className="fw-bold fs-3">
                     <Image
@@ -76,13 +99,13 @@ const Navigation = () => {
                     setLogOutModalVisible={setLogOutModalVisible}
                     isLogoutModalVisible={isLogoutModalVisible}
                 />
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="text-center">
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setIsNavbarOpen(!isNavbarOpen)} />
+                <Navbar.Collapse id="basic-navbar-nav" className="text-center" in={isNavbarOpen}>
                     <Nav className="mx-auto">
-                        <Nav.Link eventKey="1" as={Link} to="/home" className="me-1">Home</Nav.Link>
-                        <Nav.Link eventKey="2" as={Link} to="/createtask">Create Task</Nav.Link>
-                        <Nav.Link eventKey="3" as={Link} to="/topicboard" >Topic Board</Nav.Link>
-                        <Nav.Link eventKey="4" onClick={handleVisible}>Logout</Nav.Link>
+                        <Nav.Link eventKey="1" as={Link} to="/home" className="me-1" onClick={() => setIsNavbarOpen(false)}>Home</Nav.Link>
+                        <Nav.Link eventKey="2" as={Link} to="/createtask" onClick={() => setIsNavbarOpen(false)}>Create Task</Nav.Link>
+                        <Nav.Link eventKey="3" as={Link} to="/topicboard" onClick={() => setIsNavbarOpen(false)}>Topic Board</Nav.Link>
+                        <Nav.Link eventKey="4" onClick={() => { setIsNavbarOpen(false); handleVisible(); }}>Logout</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
