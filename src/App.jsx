14d9b9e4eee956b3f2c6b8e6b644e3f2f0094f27
Mitra Singh <mitra.coder@gmail.com>
2024-03-04 23:48:20 -0500

    removed commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import {
   BrowserRouter as Router,
   Routes,
-  // createBrowserRouter,
   Route,
-  // createRoutesFromElements,
-  // RouterProvider,
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import CreateTaskPage from "./pages/CreateTask/CreateTaskPage";
@@ -17,31 +14,12 @@ import PhotoUpload from "./pages/PhotoUpload/PhotoUpload";
 import TopicDetailsPage from "./pages/TopicDetails/TopicDetailsPage";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import NotFound from "./pages/NotFound/NotFound";
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/">
-//       <Route element={<ProtectedRoute />}>
-//         <Route index element={<Home />} />
-//         <Route path="createtask" element={<CreateTaskPage />} />
-//         <Route path="editprofile" element={<EditProfilePage />} />
-//         <Route path="topicboard" element={<TopicBoard />} />
-//         <Route path="topicboard/:id" element={<TopicDetailsPage />} />
-//       </Route>
-
-//       <Route path="signin" element={<SignIn />} />
-//       <Route path="signup" element={<SignUp />} />
-//       <Route path="forgotpassword" element={<ForgotPassword />} />
-//       <Route path="photoupload" element={<PhotoUpload />} />
-//       <Route path="*" element={<NotFound />} />
-//     </Route>
-//   )
-// );
+import ScrollToTop from "../src/components/ScrollToTop/ScrollToTop"
 
 function App() {
   return (
-    // <RouterProvider router={router} />
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route element={<ProtectedRoute />}>
           <Route path="/home" element={<Home />} />
