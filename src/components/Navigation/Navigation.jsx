0b5792b7de85b1f70d1dcbd58dfa06820d4579b6
Mitra Singh <mitra.coder@gmail.com>
2024-03-09 18:26:49 -0500

    refactored code for render of navigation, minor updated on styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar, Image, Form, Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { Container, Nav, Navbar, Image, Col, Tooltip, OverlayTrigger } from 'react-bootstrap';
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
@@ -13,7 +13,12 @@ import styles from "./Navigation.module.css";
 
 const Navigation = () => {
 
+    // ADDED COLUMNS TO THE NAVBAR
+
+    // State whether navbar is open or closed
     const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+    // Creates a reference referring to navbar element
     const navbarRef = useRef(null);
 
     // Handles the behavior of closing the Navbar when a user clicks outside of it
@@ -23,7 +28,10 @@ const Navigation = () => {
                 setIsNavbarOpen(false); // Close the Navbar if clicked outside
             }
         };
+        // Add an event listener to the document for click events
         document.addEventListener('click', handleOutsideClick);
+
+        // Clean up function to remove the event listener when the component unmounts
         return () => {
             document.removeEventListener('click', handleOutsideClick);
         };
@@ -56,7 +64,15 @@ const Navigation = () => {
     const handleVisible = () => setLogOutModalVisible(true);
 
     return (
-        <Navbar collapseOnSelect fixed="top" bg="info" variant="dark" expand="lg" className={`px-5 d-flex flex-row ${styles.customNav}`} ref={navbarRef}>
+        <Navbar
+            collapseOnSelect
+            fixed="top"
+            bg="info"
+            variant="dark"
+            expand="lg"
+            className={`px-5 d-flex flex-row ${styles.customNav}`}
+            ref={navbarRef}
+        >
             <Container className={styles.customContainer}>
                 <Navbar.Brand as={Link} to="/home" className="fw-bold fs-3">
                     <Image
@@ -67,7 +83,7 @@ const Navigation = () => {
                     />{" "}
                     Apollo
                 </Navbar.Brand>
-                <Form className="order-lg-last">
+                <Navbar.Brand className={`order-lg-last ${styles.brandnav2}`}>
                     <OverlayTrigger
                         key="bottom"
                         placement="bottom"
@@ -79,7 +95,7 @@ const Navigation = () => {
                     >
                         <Link to="/editprofile">
                             <Image
-                                className="object-fit-cover"
+                                className="object-fit-cover pl-2"
                                 height="35px"
                                 width="35px"
                                 src={userImage}
@@ -87,19 +103,23 @@ const Navigation = () => {
                             />
                         </Link>
                     </OverlayTrigger>
-                </Form>
+                </Navbar.Brand>
+
                 <LogoutModal
                     handleLogout={handleLogout}
                     setLogOutModalVisible={setLogOutModalVisible}
                     isLogoutModalVisible={isLogoutModalVisible}
                 />
-                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setIsNavbarOpen(!isNavbarOpen)} />
+                <Navbar.Toggle
+                    aria-controls="basic-navbar-nav"
+                    onClick={() => setIsNavbarOpen(!isNavbarOpen)}
+                />
                 <Navbar.Collapse id="basic-navbar-nav" className="text-center" in={isNavbarOpen}>
-                    <Nav className="mx-auto">
-                        <Nav.Link eventKey="1" as={Link} to="/home" className="me-1" onClick={() => setIsNavbarOpen(false)}>Home</Nav.Link>
-                        <Nav.Link eventKey="2" as={Link} to="/createtask" onClick={() => setIsNavbarOpen(false)}>Create Task</Nav.Link>
-                        <Nav.Link eventKey="3" as={Link} to="/topicboard" onClick={() => setIsNavbarOpen(false)}>Topic Board</Nav.Link>
-                        <Nav.Link eventKey="4" onClick={() => { setIsNavbarOpen(false); handleVisible(); }}>Logout</Nav.Link>
+                    <Nav className="mx-auto" onClick={() => setIsNavbarOpen(false)}>
+                        <Nav.Link eventKey="1" as={Link} to="/home" className="me-1">Home</Nav.Link>
+                        <Nav.Link eventKey="2" as={Link} to="/createtask" >Create Task</Nav.Link>
+                        <Nav.Link eventKey="3" as={Link} to="/topicboard" >Topic Board</Nav.Link>
+                        <Nav.Link eventKey="4" onClick={handleVisible}>Logout</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
