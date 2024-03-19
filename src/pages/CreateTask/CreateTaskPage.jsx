f0de92a682eb9daf8702d667390f6f5d453218fe
Mitra Singh <mitra.coder@gmail.com>
2024-03-18 22:31:34 -0400

    added: margin top

--- src/pages/CreateTask/CreateTaskPage.jsx
+++ src/pages/CreateTask/CreateTaskPage.jsx
@@ -5,7 +5,7 @@ import styles from "./CreateTaskPage.module.css";
 const CreateTaskPage = () => {
 	return (
 		<Container className={`text-light fs-6 px-4 ${styles.formContainer}`}>
-			<p className="fs-2 fw-bold d-flex justify-content-center text-light">Create Task</p>
+			<p className="fs-2 mb-4 fw-bold d-flex justify-content-center text-light">Create Task</p>
 			<CreateTaskForm />
 		</Container>
 	)
--- src/pages/EditTask/EditTaskPage.jsx
+++ src/pages/EditTask/EditTaskPage.jsx
@@ -11,7 +11,7 @@ const EditTaskPage = () => {
 
     return (
         <Container className={`text-light fs-6 px-4 ${styles.formContainer}`}>
-            <p className="fs-2 fw-bold d-flex justify-content-center text-light">Edit Your Task</p>
+            <p className="fs-2 mb-4 fw-bold d-flex justify-content-center text-light">Edit Your Task</p>
             <ErrorBoundary FallbackComponent={ErrorFallbackProfile}>
                 <EditTaskForm task={task} creatorName={creatorName} creatorPhoto={creatorPhoto} />
             </ErrorBoundary>
