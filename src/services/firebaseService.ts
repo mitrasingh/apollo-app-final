73c798783fd0cacd6c9715572bd14c66b9619771
mitrasingh <mitra.coder@gmail.com>
2025-04-18 10:48:33 -0400

    feat: add login and logout dummy methods for testing

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -1,75 +1,29 @@
-import {
-	browserSessionPersistence,
-	setPersistence,
-	signInWithEmailAndPassword,
-} from "firebase/auth";
-import { doc, getDoc } from "firebase/firestore";
-import { db, auth } from "../utils/firebase-config";
-import { useNavigate, Link } from "react-router-dom";
-import { toast } from "react-toastify";
-import { useDispatch } from "react-redux";
-import { loginUser } from "../store/user/userSlice";
+import { auth } from "../utils/firebase-config";
 import { SignInProps } from "../models/SignInProps";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-// Redux function which will dispatch actions needed for user state changes
-// const dispatch = useDispatch();
-
-// Instance created from firebase authentication
-// const userAuth = auth;
-
-// React router function allows user to navigate to specified route
-// const navigate = useNavigate();
-
-const signIn = async (data: SignInProps) => {
-	return console.log(data);
-};
-
-/*
-const signIn = async (data: SignInProps) => {
+const login = async (data: SignInProps) => {
 	try {
-		if (!userAuth.currentUser) {
-			console.log("No user found");
-		} else {
-			// Sets parameters for user to be logged out if window/tab is closed
-			await setPersistence(userAuth, browserSessionPersistence);
-			await signInWithEmailAndPassword(userAuth, data.email, data.password);
-			const docRef = doc(db, "users", userAuth.currentUser.uid);
-			const docSnap = await getDoc(docRef);
-			const userData = docSnap.data();
-			// Assigns default values to Redux user state
-			dispatch(
-				loginUser({
-					userId: userAuth.currentUser.uid,
-					userPhoto: userData?.userPhoto,
-					firstName: userAuth.currentUser.displayName,
-					lastName: userData?.lastname,
-					title: userData?.title,
-					email: userAuth.currentUser.email,
-				})
-			);
-			toast.success(
-				`Hell ${userAuth.currentUser.displayName}, you are logged in!`,
-				{
-					hideProgressBar: true,
-				}
-			);
-			navigate("/home");
-		}
-	} catch (error) {
-		if (error instanceof Error) {
-			if (error.message.includes("user-not-found")) {
-				toast.error("User not found!");
-			} else if (error.message.includes("wrong-password")) {
-				toast.error("Password is incorrect!");
-			} else {
-				toast.error("Sorry, we are having some technical issues!");
-			}
-			console.log(`Error: ${error.message}`);
-		} else {
-			toast.error("Strange error has occurred!");
-		}
+		const userCredential = await signInWithEmailAndPassword(
+			auth,
+			data.email,
+			data.password
+		);
+		return console.log(
+			`User name ${auth.currentUser?.displayName} is logged in!`
+		);
+	} catch (error: any) {
+		throw new Error(error.message);
 	}
 };
-*/
 
-export { signIn };
+const logOut = async () => {
+	try {
+		await signOut(auth);
+		return console.log(`User logged out ${auth.currentUser?.displayName}`);
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
+};
+
+export { login, logOut };
