342db15ad8c42036deddb6300358b00709382e64
mitrasingh <mitra.coder@gmail.com>
2025-05-05 18:59:38 -0400

    feat: add useNavigate from react router dom, implement into login and gustlogin

--- src/services/useAuth.ts
+++ src/services/useAuth.ts
@@ -4,10 +4,15 @@ import { useDispatch } from "react-redux";
 import { loginUser } from "./../store/user/userSlice";
 import { SignInProps } from "../models/SignInProps";
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { toast } from "react-toastify";
+import { useNavigate, Link } from "react-router-dom";
 
 export const useAuth = () => {
 	// Redux function which will dispatch actions needed for user state changes
 	const dispatch = useDispatch();
+
+	// React router function allows user to navigate to specified route
+	const navigate = useNavigate();
 
 	const login = async (data: SignInProps) => {
 		try {
@@ -30,9 +35,16 @@ export const useAuth = () => {
 						email: auth.currentUser.email,
 					})
 				);
+				// toast.success(
+				// 	`Hello ${auth.currentUser?.displayName}, you are logged in!`,
+				// 	{
+				// 		hideProgressBar: true,
+				// 	}
+				// );
 			}
 
-			return console.log(`User name ${auth.currentUser?.displayName}`);
+			console.log(`User name ${auth.currentUser?.displayName}`);
+			navigate("/home");
 		} catch (error: any) {
 			throw new Error(error.message);
 		}
@@ -54,9 +66,8 @@ export const useAuth = () => {
 				"guest@apollo.com",
 				"guest123"
 			);
-			return console.log(
-				`User name ${auth.currentUser?.displayName} is logged in!`
-			);
+			console.log(`User name ${auth.currentUser?.displayName} is logged in!`);
+			navigate("/home");
 		} catch (error: any) {
 			throw new Error(error.message);
 		}
