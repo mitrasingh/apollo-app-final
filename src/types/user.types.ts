d178330531e94ccd1620dd3212fdc525226e8511
mitrasingh <mitra.coder@gmail.com>
2025-05-07 20:22:50 -0400

    feat: new types folder created, includes type definition for signin data and user data'

---
+++ src/types/user.types.ts
@@ -0,0 +1,8 @@
+export interface User {
+	userId: string | null;
+	userPhoto: string | null;
+	firstName: string | null;
+	lastName: string | null;
+	title: string | null;
+	email: string | null;
+}
