cc31f32d8d068f77bd3c3d68302edda5ba6954d7
mitrasingh <mitra.coder@gmail.com>
2025-05-18 22:06:03 -0400

    feat: add new function which will preview photo of user selected file, import firebase storage functions

--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -1,1 +1,33 @@
-export const profileService = () => {};
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { auth } from "../utils/firebase-config";
+import { toast } from "react-toastify";
+
+export const profileService = () => {
+	// Firebase storage access
+	const storage = getStorage();
+	const storageRef = ref(storage);
+
+	//Function for previewing photo
+	const previewProfilePhoto = async (data: Blob) => {
+		try {
+			if (auth.currentUser) {
+				const imageRef = ref(
+					storageRef,
+					`user-photo/temp-${auth.currentUser.uid}`
+				);
+				await uploadBytes(imageRef, data);
+				const getURL = await getDownloadURL(imageRef);
+				return getURL;
+			}
+		} catch (error: any) {
+			console.log(`Error: ${error.message}`);
+			toast.error("Please try another photo!", {
+				hideProgressBar: true,
+			});
+		}
+	};
+	//function for actually uploading the photo
+	const submitProfilePhoto = async () => {};
+
+	return { previewProfilePhoto, submitProfilePhoto };
+};
