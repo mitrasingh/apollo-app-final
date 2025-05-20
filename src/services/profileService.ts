c8cae0180ccf9fbec862599e9ac0ed824945d8b1
mitrasingh <mitra.coder@gmail.com>
2025-05-20 13:19:01 -0400

    refactor: remove toast, redux and navigate from file, add new errors for functions

--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -1,83 +1,64 @@
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { auth, db } from "../utils/firebase-config";
-import { useDispatch, useSelector } from "react-redux";
-import type { RootState } from "../store/store";
-import { useNavigate } from "react-router-dom";
-import { loginUser } from "../store/user/userSlice";
-
-import { toast } from "react-toastify";
 
 export const profileService = () => {
-	// React router function allows user to navigate to specified route
-	const navigate = useNavigate();
-
 	// Firebase storage access
 	const storage = getStorage();
 	const storageRef = ref(storage);
 
-	// Redux management
-	const userState = useSelector((state: RootState) => state.user); // Access to Redux initial user state properties
-	const dispatch = useDispatch(); // Redux function which dispatches actions to Redux store
-
 	//Function for previewing photo
-	const previewProfilePhoto = async (data: Blob) => {
+	const previewProfilePhoto = async (file: Blob) => {
 		try {
-			if (auth.currentUser) {
+			if (!auth.currentUser) {
+				throw new Error("User not authenticated.");
+			} else {
 				const imageRef = ref(
 					storageRef,
 					`user-photo/temp-${auth.currentUser.uid}`
 				);
-				await uploadBytes(imageRef, data);
+				await uploadBytes(imageRef, file);
 				const getURL = await getDownloadURL(imageRef);
 				return getURL;
 			}
 		} catch (error: any) {
-			console.log(`Error: ${error.message}`);
-			toast.error("Please try another photo!", {
-				hideProgressBar: true,
-			});
+			throw error;
 		}
 	};
 	//function for actually uploading the photo
-	const submitProfilePhoto = async (chosenFile: Blob) => {
+	const submitProfilePhoto = async (file: Blob) => {
 		try {
-			if (!chosenFile) {
-				toast.error("Photo is required!", {
-					hideProgressBar: true,
+			if (!file) {
+				throw new Error("Photo is required!");
+			}
+			if (!auth.currentUser) {
+				throw new Error("User not authenticated.");
+			}
+			const imageRef = ref(storageRef, `user-photo/${auth.currentUser.uid}`);
+			await uploadBytes(imageRef, file);
+			const userPhotoURL = await getDownloadURL(
+				ref(storageRef, `user-photo/${auth.currentUser.uid}`)
+			);
+			const docRef = doc(db, "users", auth.currentUser.uid);
+			const docSnap = await getDoc(docRef);
+			if (userPhotoURL && docSnap) {
+				const data = docSnap.data();
+				await updateDoc(docRef, {
+					userPhoto: data,
 				});
-			} else if (auth.currentUser) {
-				const imageRef = ref(storageRef, `user-photo/${auth.currentUser.uid}`);
-				await uploadBytes(imageRef, chosenFile);
-				const userPhotoURL = await getDownloadURL(
-					ref(storageRef, `user-photo/${auth.currentUser.uid}`)
-				);
-				const docRef = doc(db, "users", auth.currentUser.uid);
-				const docSnap = await getDoc(docRef);
-				if (auth && userPhotoURL && docSnap) {
-					const data = docSnap.data();
-					await updateDoc(docRef, {
-						userPhoto: data,
-					});
-					dispatch(
-						loginUser({
-							userId: auth.currentUser.uid,
-							userPhoto: userPhotoURL,
-							firstName: auth.currentUser.displayName,
-							lastName: data?.lastname,
-							title: data?.title,
-							email: auth.currentUser.email,
-						})
-					);
-				}
-				toast.success(`Welcome ${auth.currentUser.displayName} to Apollo!`);
-				navigate("/home");
+				return {
+					userId: auth.currentUser.uid,
+					userPhoto: userPhotoURL,
+					firstName: auth.currentUser.displayName,
+					lastName: data?.lastname,
+					title: data?.title,
+					email: auth.currentUser.email,
+				};
+			} else {
+				throw new Error("User data not found.");
 			}
 		} catch (error: any) {
-			console.log(`Error: ${error.message}`);
-			toast.error("Sorry, we are having some technical issues!", {
-				hideProgressBar: true,
-			});
+			throw error;
 		}
 	};
 
