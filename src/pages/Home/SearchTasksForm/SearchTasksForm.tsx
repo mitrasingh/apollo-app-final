84cb9f4acbcd534ec5567308dd1d0e53bbf2cb2a
mitrasingh <mitra.coder@gmail.com>
2025-06-04 21:43:09 -0400

    refactor: simplify component, lessen props being passed, component has now has its own input state (onInputChange)

--- src/pages/Home/SearchTasksForm/SearchTasksForm.tsx
+++ src/pages/Home/SearchTasksForm/SearchTasksForm.tsx
@@ -1,67 +1,37 @@
-import {
-	Col,
-	Container,
-	Form,
-	FormControl,
-	InputGroup,
-	Row,
-	Button,
-} from "react-bootstrap";
+import { Col, Container, FormControl, InputGroup, Row } from "react-bootstrap";
 
-interface SearchTasksFormProps {
+type Props = {
 	userInput: string;
-	setUserInput: (value: string) => void;
-	filterSearchHandle: (
-		event: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLImageElement>
-	) => void;
-	refreshTasksHandle: () => void;
-}
+	onInputChange: (value: string) => void;
+};
 
 // Prop from Home.jsx
-const SearchTasksForm = ({
-	userInput,
-	setUserInput,
-	filterSearchHandle,
-	refreshTasksHandle,
-}: SearchTasksFormProps) => {
-	// Function sets value from user input within form field
-	const handleUserInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setUserInput(e.target.value);
-	};
-
-	// Function resets tasks when user backspaces/deletes all values from form field
-	const handleKeyDownReset = (e: React.KeyboardEvent<HTMLInputElement>) => {
-		if (e.keyCode === 8 && userInput.length === 1) return refreshTasksHandle();
-		if (e.keyCode === 46 && userInput.length === 1) return refreshTasksHandle();
-	};
+const SearchTasksForm = ({ userInput, onInputChange }: Props) => {
+	console.log("search is rendered");
 
 	return (
 		<Container className="mt-4 mb-3">
 			<Row className="justify-content-center">
 				<Col lg={6} md={6} sm={8}>
-					<Form onSubmit={filterSearchHandle} className="d-flex ms-2">
-						<InputGroup>
-							<Button>
-								<img
-									src="/search_new.svg"
-									width="20"
-									height="20"
-									alt="Search bar button"
-									onClick={filterSearchHandle}
-								/>
-							</Button>
-							<FormControl
-								className="me-2 fs-6 shadow-none"
-								type="text"
-								name="tasksearch"
-								autoComplete="tasksearch"
-								placeholder="Search by task name..."
-								value={userInput}
-								onKeyDown={handleKeyDownReset}
-								onChange={handleUserInput}
+					<InputGroup className="d-flex ms-2">
+						<InputGroup.Text>
+							<img
+								src="/search_new.svg"
+								width="20"
+								height="20"
+								alt="Search bar button"
 							/>
-						</InputGroup>
-					</Form>
+						</InputGroup.Text>
+						<FormControl
+							className="me-2 fs-6 shadow-none"
+							type="text"
+							name="tasksearch"
+							autoComplete="tasksearch"
+							placeholder="Search by task name..."
+							value={userInput}
+							onChange={(e) => onInputChange(e.target.value)}
+						/>
+					</InputGroup>
 				</Col>
 			</Row>
 		</Container>
