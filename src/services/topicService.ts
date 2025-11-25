d1ccc50b1bec335c040273a50240118c4adbeb41
mitrasingh <mitra.coder@gmail.com>
2025-11-25 18:51:30 -0500

    feat: add total count function

--- src/services/topicService.ts
+++ src/services/topicService.ts
@@ -1,23 +1,24 @@
 import { collection, getDocs, query, orderBy, limit, startAfter } from "firebase/firestore";
-import { TopicData } from "../types/topicdata.types";
 import { db } from "../utils/firebase-config";
+import { TopicData } from "../types/topicdata.types";
+import { getCountFromServer } from "firebase/firestore";
 
-// Fetch all topics
-// export const fetchAllTopics = async (): Promise<TopicData[]> => {
-// 	const dbRef = collection(db, "topics");
-// 	const snapshot = await getDocs(query(dbRef));
-// 	return snapshot.docs.map((doc) => ({
-// 		...(doc.data() as Omit<TopicData, "topicId">),
-// 		topicId: doc.id,
-// 	}));
-// };
+// Fetch the total count of topics
+export const fetchTopicsCount = async (): Promise<number> => {
+	const dbRef = collection(db, "topics");
+	const countSnapshot = await getCountFromServer(query(dbRef));
+	return countSnapshot.data().count;
+};
 
-export const fetchInitialTopics = async (): Promise<{
+// Fetch the first batch of topics
+export const fetchInitialTopics = async (
+	limitCount: number = 6
+): Promise<{
 	topics: TopicData[];
 	lastDoc: any;
 }> => {
 	const dbRef = collection(db, "topics");
-	const topicsQuery = query(dbRef, orderBy("datePosted", "desc"), limit(6));
+	const topicsQuery = query(dbRef, orderBy("datePosted", "desc"), limit(limitCount));
 	const snapshot = await getDocs(topicsQuery);
 
 	const topics = snapshot.docs.map((doc) => ({
@@ -31,6 +32,7 @@ export const fetchInitialTopics = async (): Promise<{
 	};
 };
 
+// Fetch more topics for pagination
 export const fetchMoreTopics = async (
 	lastDoc: any,
 	limitCount: number = 6
